2c612f16485853286dad567c571a686e3411f071
ivan.dario <ivan.dario@nexiona.com>
2022-03-05 09:42:10 +0100

    Add initial folder structure and react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Welcome } from "./presentation/Welcome";
+import { Main } from "./presentation/Main";
 
 function App(): JSX.Element {
-    return <div className="App">Welcome!</div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/welcome" element={<Welcome />} />
+                <Route path="/" element={<Main />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/presentation/Main.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export function Main(): JSX.Element {
+    return <div>Main Page</div>;
+}
